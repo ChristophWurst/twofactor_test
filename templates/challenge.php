cdc91b289c51285cbbf6e950dda2c2f39386b31c
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-06-01 15:41:29 +0200

    prevent browser autocomplete

--- templates/challenge.php
+++ templates/challenge.php
@@ -1,5 +1,5 @@
 <form method="POST">
 	<input type="hidden" name="redirect_url" value="<?php p($_['redirect_url']); ?>">
-	<input type="text" name="challenge" value="passme" required="required">
+	<input type="text" name="challenge" autocomplete="off" autocapitalize="off" value="passme" required="required">
 	<input type="submit" class="button" value="Verify">
 </form>
